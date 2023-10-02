603708d417abb961e598acb4901ca00a05f0c4db
Kushal Seth <kushalseth1408@gmail.com>
2023-10-03 05:18:34 +0530

    integrated search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,5 @@
 import RestaurantCard from "./RestaurantCard";
-import resData from "../utils/mockData";
+import mockData from "../utils/mockData";
 import { useState, useEffect } from "react";
 import { SWIGGY_API_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
@@ -7,6 +7,13 @@ import Shimmer from "./Shimmer";
 const Body = () => {
   // local state variable
   const [listOfRestaurants, setListOfResturants] = useState([]);
+  const [filteredRestuarants, setFilteredRestuarants] = useState([]);
+
+  const [searchText, setSearchText] = useState("");
+
+  const enableActualApiCall = false;
+
+  console.log("body rendered");
 
   useEffect(() => {
     fetchData();
@@ -18,31 +25,46 @@ const Body = () => {
     const json = await data.json();
     const formattedRestaurants = buildRestaurantObject(json);
     setListOfResturants(formattedRestaurants);
+    setFilteredRestuarants(formattedRestaurants);
   };
 
   const buildRestaurantObject = (json) => {
-    const cards = json?.data?.cards;
-    let resData = [];
     let result = [];
+    if (enableActualApiCall) {
+      const cards = json?.data?.cards;
+      let resData = [];
 
-    for (var i = 0; i < cards?.length; i++) {
-      if (
-        cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.length >
-        0
-      ) {
-        resData =
-          cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-        break;
+      for (var i = 0; i < cards?.length; i++) {
+        if (
+          cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+            ?.length > 0
+        ) {
+          resData =
+            cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+          break;
+        }
       }
-    }
 
-    if (resData?.length > 0) {
-      resData.forEach((restaurant) => {
-        result.push(restaurant?.info);
-      });
+      if (resData?.length > 0) {
+        resData.forEach((restaurant) => {
+          result.push(restaurant?.info);
+        });
+      }
+    } else {
+      result = mockData;
     }
 
     return result;
+  };
+
+  const searchTextHandler = () => {
+    console.log(searchText);
+    debugger;
+    const filteredRestuarants = listOfRestaurants.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    setFilteredRestuarants(filteredRestuarants);
   };
 
   return listOfRestaurants?.length == 0 ? (
@@ -50,19 +72,37 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
-        <button
-          className="filter-btn"
-          onClick={() => {
-            tt = resData.filter((x) => x.avgRating > 4);
-            setListOfResturants(tt);
-          }}
-        >
-          Top Rated Restaurants
-        </button>
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            className="btn-search"
+            onClick={() => {
+              searchTextHandler();
+            }}
+          >
+            Search
+          </button>
+          <button
+            className="filter-btn"
+            onClick={() => {
+              tt = resData.filter((x) => x.avgRating > 4);
+              setListOfResturants(tt);
+            }}
+          >
+            Top Rated Restaurants
+          </button>
+        </div>
       </div>
 
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestuarants.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
