46f18b967117a813a464ffe8095d0efc242150af
Kushal Seth <kushalseth1408@gmail.com>
2023-10-03 15:42:36 +0530

    integrated routers

---
+++ src/components/ContactUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactUs = () => {
+  return <div className="contact-us-container">Contact Us</div>;
+};
+
+export default ContactUs;
---
+++ src/components/Error.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const Error = () => {
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div className="error-container">
+      <h1> OOPS </h1>
+      <h2> Something went wrong </h2>
+      <p>
+        {err.status} : {err.statusText}
+      </p>
+    </div>
+  );
+};
+
+export default Error;
