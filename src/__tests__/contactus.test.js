adc7c943ec3a27c16a6a90bb477e2207cef626cc
Kushal Seth <kushalseth1408@gmail.com>
2023-10-18 02:46:55 +0530

    added test cases

--- src/__tests__/contactus.test.js
+++ src/__tests__/contactus.test.js
@@ -2,10 +2,30 @@ import ContactUs from "../components/ContactUs";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
-test("should load contactUs component", () => {
-  render(<ContactUs />);
+describe("these are Contact Us test cases", () => {
+  it("should load contactUs component", () => {
+    render(<ContactUs />);
 
-  const headings = screen.getByRole("heading");
+    const headings = screen.getByRole("heading");
 
-  expect(headings).toBeInTheDocument();
+    expect(headings).toBeInTheDocument();
+  });
+
+  test("should load button inside contact us", () => {
+    render(<ContactUs />);
+
+    const button = screen.getByText("Submit");
+
+    expect(button).toBeInTheDocument();
+  });
+
+  test("should load 2 input boxes", () => {
+    render(<ContactUs />);
+
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    console.log("kushal ", inputBoxes.length);
+
+    expect(inputBoxes.length).toBe(2);
+  });
 });
